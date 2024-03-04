f967b92e44565877e3220910aec5b534f7b11770
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-03-03 23:06:35 -0300

    Adding Swagger

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 
 app = FastAPI()
 
@@ -6,3 +7,13 @@ app = FastAPI()
 @app.get("/screen-shot/")
 def screen_shot():
     return {"Hello": "In the future, this will return a screen shot of a website."}
+
+
+@app.get("/", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="FastAPI Swagger UI")
+
+
+@app.get("/openapi.json", include_in_schema=False)
+async def get_open_api_endpoint():
+    return app.openapi()
